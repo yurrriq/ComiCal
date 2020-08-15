01a173be00f23eee0ae1de16236ad3be896beb29
Eric Bailey <eric@ericb.me>
2020-08-15 16:38:40 -0500

    Prefer callCabal2nix

--- Makefile
+++ Makefile
@@ -1,10 +1,6 @@
 .PHONY: build
-build: release.nix default.nix $(wildcard src/**.hs) $(wildcard app/**.hs)
+build: default.nix $(wildcard src/**.hs) $(wildcard app/**.hs)
 	@ nix build -f $<
-
-
-default.nix: package.yaml LICENSE VERSION
-	@ cabal2nix . --maintainer yurrriq --hpack >$@
 
 
 .PHONY: update
--- default.nix
+++ default.nix
@@ -1,43 +1,5 @@
-{ mkDerivation
-, base
-, bytestring
-, hpack
-, lens
-, modern-uri
-, optparse-applicative
-, req
-, stdenv
-, tagsoup
-, text
-, time-compat
-}:
-mkDerivation {
-  pname = "ComiCal";
-  version = "1.0.0.0";
-  src = ./.;
-  isLibrary = true;
-  isExecutable = true;
-  libraryHaskellDepends = [
-    base
-    bytestring
-    lens
-    modern-uri
-    req
-    tagsoup
-    text
-    time-compat
-  ];
-  libraryToolDepends = [ hpack ];
-  executableHaskellDepends = [
-    base
-    bytestring
-    lens
-    optparse-applicative
-    text
-  ];
-  prePatch = "hpack";
-  homepage = "https://github.com/yurrriq/ComiCal#readme";
-  description = "Track the publish dates of your favorite comics";
-  license = stdenv.lib.licenses.mit;
-  maintainers = with stdenv.lib.maintainers; [ yurrriq ];
-}
+{ pkgs ? import ./nix }:
+let
+  src = pkgs.nix-gitignore.gitignoreRecursiveSource [ ".git/" ] ./.;
+in
+pkgs.haskellPackages.callCabal2nix "ComiCal" src {}
